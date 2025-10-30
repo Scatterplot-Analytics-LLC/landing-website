d8af91c25d013b99e9831209f0bdc20ac01dea78
KhubiAubergine <khubi@aubergine.co>
2025-10-30 11:44:32 +0530

    SPL-241: FEATURE Add next-sitemap configuration and generate robots.txt and sitemap files

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://scatterplot-landing.netlify.app/',
+  generateRobotsTxt: true, // auto creates robots.txt too
+}
